6c9078b6f63629419085747326ebcf1044d63046
66ds <1532498760@qq.com>
2020-11-17 22:34:58 +0800

    '17'

--- src/main/java/com/qianbing/blog/controller/ArticlesPubController.java
+++ src/main/java/com/qianbing/blog/controller/ArticlesPubController.java
@@ -1,9 +1,11 @@
 package com.qianbing.blog.controller;
 
+import com.qianbing.blog.entity.ArticlesEntity;
 import com.qianbing.blog.service.ArticlesService;
 import com.qianbing.blog.utils.PageUtils;
 import com.qianbing.blog.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +32,14 @@ public class ArticlesPubController {
     public R list(@RequestBody Map<String, Object> params) {
         PageUtils page = articlesService.queryPage(params);
         return R.ok().put("data", page);
+    }
+
+    /**
+     * 查询单篇文章信息
+     */
+    @RequestMapping("/info/{articleId}")
+    public R info(@PathVariable("articleId") Long articleId) {
+        ArticlesEntity articles = articlesService.findArticleById(articleId);
+        return R.ok().put("data", articles);
     }
 }
